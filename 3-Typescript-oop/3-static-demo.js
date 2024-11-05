95b8111a6d1210287acc0d7ab647e0e395a52beb
Ashok S <ashok.s@koenig-solutions.com>
2024-11-05 10:31:30 +0530

    static properties

---
+++ 3-Typescript-oop/3-static-demo.js
@@ -0,0 +1,54 @@
+var Bank = /** @class */ (function () {
+    // Contructor of the class
+    function Bank(_accountName, _accountBalance) {
+        Bank.lastAccountNumber++;
+        this.accountNumber = Bank.lastAccountNumber;
+        this._accountName = _accountName;
+        this._accountBalance = _accountBalance;
+    }
+    Object.defineProperty(Bank.prototype, "accountBalance", {
+        // Methods of the class
+        get: function () {
+            return this._accountBalance;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Bank.prototype, "accountName", {
+        get: function () {
+            return this._accountName;
+        },
+        set: function (name) {
+            this._accountName = name;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Bank.prototype.deposit = function (amount) {
+        if (amount > 0) {
+            this._accountBalance += amount;
+            console.log('Deposit successful');
+        }
+        else {
+            console.log('Invalid amount');
+        }
+    };
+    Bank.prototype.withdraw = function (amount) {
+        if (this._accountBalance >= amount) {
+            this._accountBalance -= amount;
+            console.log('Withdrawal successful');
+        }
+        else {
+            console.log('Insufficiant balance');
+        }
+    };
+    Bank.prototype.displayBalance = function () {
+        console.log("".concat(this._accountName, "-").concat(this.accountNumber, " balance is ").concat(this._accountBalance));
+    };
+    Bank.lastAccountNumber = 100;
+    return Bank;
+}());
+var acc1 = new Bank('John Peter', 1000);
+acc1.displayBalance();
+var acc2 = new Bank('John Doe', 2000);
+acc2.displayBalance();
---
+++ 3-Typescript-oop/3-static-demo.ts
@@ -0,0 +1,48 @@
+class Bank{
+    // Properties of the class
+    private accountNumber:number;
+    private _accountName: string;
+    private _accountBalance:number;
+    static lastAccountNumber: number = 100
+    // Contructor of the class
+    constructor(_accountName: string, _accountBalance:number){
+        Bank.lastAccountNumber++
+        this.accountNumber = Bank.lastAccountNumber
+        this._accountName= _accountName
+        this._accountBalance = _accountBalance
+    }
+    // Methods of the class
+    public get accountBalance(): number{
+        return this._accountBalance;
+    }
+    public set accountName(name: string){
+        this._accountName = name;
+    }
+    public get accountName(): string{
+        return this._accountName
+    }
+    public deposit(amount: number): void{
+        if(amount>0){
+            this._accountBalance +=amount
+            console.log('Deposit successful');            
+        }else{
+            console.log('Invalid amount');            
+        }
+    }
+   public withdraw(amount: number):void{
+        if(this._accountBalance>=amount){
+            this._accountBalance -=amount;
+            console.log('Withdrawal successful');            
+        }else{
+            console.log('Insufficiant balance');            
+        }
+    }
+   public displayBalance():void{
+        console.log(`${this._accountName}-${this.accountNumber} balance is ${this._accountBalance}`);        
+    }
+}
+const acc1 = new Bank('John Peter', 1000)
+acc1.displayBalance()
+
+const acc2 = new Bank('John Doe', 2000)
+acc2.displayBalance()
