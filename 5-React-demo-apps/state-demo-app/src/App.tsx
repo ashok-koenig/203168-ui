f674e6da43ddd95b82efb8dc10ecddeb2e2eb674
Ashok S <ashok.s@koenig-solutions.com>
2024-11-07 10:31:15 +0530

    with use Effect example

--- 5-React-demo-apps/state-demo-app/src/App.tsx
+++ 5-React-demo-apps/state-demo-app/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -9,12 +9,29 @@ function App() {
   const [count, setCount] = useState<number>(0)
   const [name, setName] = useState<string>('John')
   const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  useEffect(()=>{
+    const currentName = localStorage.getItem('name') || 'John'
+    const currentCount = Number(localStorage.getItem('count') || "0")
+    setName(currentName)
+    setCount(currentCount)    
+  }, [])
+
+  useEffect(()=>{
+    // console.log("useEffect called..."); 
+    localStorage.setItem('count', String(count))   
+  },[count])
+
+  useEffect(()=>{
+        localStorage.setItem('name', name)   
+  },[name])
+
   return (
     <>
       <h1>Working with State management</h1>
       <button onClick={()=>setIsLoggedIn((status)=> !status)}>{isLoggedIn?'Logout':'Login'}</button>
-      <Movie />
-      <FriendsList />
+      {/* <Movie />
+      <FriendsList /> */}
       <hr/>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
