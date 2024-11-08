1c0badcb9ca14db2a8e2a644510cecba4b7360f4
Ashok S <ashok.s@koenig-solutions.com>
2024-11-08 10:46:30 +0530

    WIth state based validation

--- 5-React-demo-apps/forms-demo-app/src/components/StateFrom.tsx
+++ 5-React-demo-apps/forms-demo-app/src/components/StateFrom.tsx
@@ -7,13 +7,26 @@ export default function StateFrom() {
         gender: '',
         course: ''
     })
+    const [errors, setErrors] = useState<{ [key: string]: string}>({})
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) =>{
         const {name, value} = e.target
         setFormData((preData)=> ({...preData, [name]:value}))
     }
+    const validateFrom = () =>{
+        const currentErrors: {[key: string]: string} = {}
+        if(!formData.name) currentErrors.name = 'Name is required'
+        if(!formData.email) currentErrors.email = 'Email is required'
+        if(!formData.gender) currentErrors.gender = 'Gender is required'
+        if(!formData.course) currentErrors.course = 'Course is required'
+        setErrors(currentErrors)
+        return Object.keys(currentErrors).length === 0
+    }
     const handleSubmit = (e: React.FormEvent) =>{
         e.preventDefault();
-        console.log(formData);        
+        if(validateFrom()){
+             console.log(formData);     
+        }
+          
     }
   return (
     <div>
@@ -22,12 +35,12 @@ export default function StateFrom() {
         <div>
             <label>Name:</label>
             <input type="text" name="name" value={formData.name} onChange={handleChange} placeholder='Enter Name' />
-            <span className="error"></span>
+            {errors.name && <span className="error">{errors.name}</span>}
         </div>
         <div>
             <label>Email:</label>
             <input type="email" name="email" value={formData.email} onChange={handleChange} placeholder='Enter Email' />
-            <span className="error"></span>
+            {errors.email && <span className="error">{errors.email}</span>}
         </div>
         <div>
             <label>Gender:</label>
@@ -37,7 +50,7 @@ export default function StateFrom() {
             <label>
                 <input type="radio" name="gender" value="female" checked={formData.gender == 'female'} onChange={handleChange} /> Female
             </label>
-            <span className="error"></span>
+            {errors.gender && <span className="error">{errors.gender}</span>}
         </div>
         <div>
             <label>Course:</label>
@@ -47,7 +60,7 @@ export default function StateFrom() {
                 <option value="TypeScript">TypeScript</option>
                 <option value="React">React</option>
             </select>
-            <span className="error"></span>
+            {errors.course && <span className="error">{errors.course}</span>}
         </div>
         <div>
             <button type="submit">Register</button>
