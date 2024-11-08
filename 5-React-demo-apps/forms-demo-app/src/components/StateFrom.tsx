b37e0fe8df8f978159f5bc4194e3b26dec5e1e2c
Ashok S <ashok.s@koenig-solutions.com>
2024-11-08 10:31:28 +0530

    handle change and handle submit

--- 5-React-demo-apps/forms-demo-app/src/components/StateFrom.tsx
+++ 5-React-demo-apps/forms-demo-app/src/components/StateFrom.tsx
@@ -11,33 +11,37 @@ export default function StateFrom() {
         const {name, value} = e.target
         setFormData((preData)=> ({...preData, [name]:value}))
     }
+    const handleSubmit = (e: React.FormEvent) =>{
+        e.preventDefault();
+        console.log(formData);        
+    }
   return (
     <div>
     <h1>Register Form using state</h1>
-    <form>
+    <form onSubmit={handleSubmit}>
         <div>
             <label>Name:</label>
-            <input type="text" name="name" placeholder='Enter Name' />
+            <input type="text" name="name" value={formData.name} onChange={handleChange} placeholder='Enter Name' />
             <span className="error"></span>
         </div>
         <div>
             <label>Email:</label>
-            <input type="email" name="email" placeholder='Enter Email' />
+            <input type="email" name="email" value={formData.email} onChange={handleChange} placeholder='Enter Email' />
             <span className="error"></span>
         </div>
         <div>
             <label>Gender:</label>
             <label>
-                <input type="radio" name="gender" value="male" /> Male
+                <input type="radio" name="gender" value="male" checked={formData.gender == 'male'} onChange={handleChange}/> Male
             </label>
             <label>
-                <input type="radio" name="gender" value="female" /> Female
+                <input type="radio" name="gender" value="female" checked={formData.gender == 'female'} onChange={handleChange} /> Female
             </label>
             <span className="error"></span>
         </div>
         <div>
             <label>Course:</label>
-            <select name="course">
+            <select name="course" value={formData.course} onChange={handleChange}>
                 <option value="">Select Course</option>
                 <option value="JavaScript">JavaScript</option>
                 <option value="TypeScript">TypeScript</option>
